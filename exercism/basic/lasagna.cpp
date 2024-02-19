60acde3b3eb16898312f8c6e600c87930a1dd51a
Eufrausino <gustavo.medeiros@ufv.br>
2024-02-19 14:25:48 -0300

    resolução da tarefa

--- exercism/basic/lasagna.cpp
+++ exercism/basic/lasagna.cpp
@@ -2,9 +2,35 @@
 
 using namespace std;
 
+int ovenTime()
+{
+    return 40;
+}
+
+int remainingOvenTime(int actualMinutesInOven)
+{
+    return ovenTime() - actualMinutesInOven;
+}
+
+int preparationTime(int numberOfLayers)
+{
+    return numberOfLayers*2;
+}
+
+int elapsedTime(int numberOfLayers, int actualMinutesInOven)
+{
+    return preparationTime(numberOfLayers) + actualMinutesInOven;
+}
+
 int main()
 {
-    
+    int layers = 2;
+    int actualMinutesInOven = 20;
+
+    cout << ovenTime() << endl;
+    cout << remainingOvenTime(actualMinutesInOven) << endl;
+    cout << preparationTime(layers) << endl;
+    cout << elapsedTime(layers, actualMinutesInOven) << endl;
     
     return 0;
 }
